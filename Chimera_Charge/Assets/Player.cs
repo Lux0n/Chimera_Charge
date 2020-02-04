dc843a892892605ca98e060ed789a5798edc9eda
Kronosz12 <kunszabi10@gmail.com>
2020-02-04 18:56:22 +0100

    Player movement
    Jobbra,ballra mozgás kész

---
+++ Chimera_Charge/Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 1f; 
+    private void Update()
+    {
+        float translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(0,translation,0);
+    }
+}
