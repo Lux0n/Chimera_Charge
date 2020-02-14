39e907df95a4bd761f3f191a1424393d4daa7624
Kronosz12 <kunszabi10@gmail.com>
2020-02-14 20:03:34 +0100

    Shooting Added
    Changed Player speed
    Projectile added

---
+++ Chimera_Charge/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float sideBoundary = 1f;
+
+    Vector3 minScreenBounds;
+    Vector3 maxScreenBounds;
+
+    private void Start()
+    {
+        minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+    }
+
+    private void Update()
+    {
+        float translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(0,translation,0);
+        
+        Vector3 clampedposition = transform.position;
+        clampedposition.x = Mathf.Clamp(clampedposition.x, minScreenBounds.x + sideBoundary, maxScreenBounds.x - sideBoundary);
+        transform.position = clampedposition;
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var bullet = Instantiate(bulletPrefab, new Vector3(transform.position.x,transform.position.y + 1,2), Quaternion.identity);
+        }
+    }
+}
---
+++ Chimera_Charge/Assets/Scripts/Player_Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] int damage = 100;
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + (speed * Time.deltaTime));
+    }
+}
